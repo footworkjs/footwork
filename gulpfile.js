dacea94d7d48e204858f7b9f764e86c71f044361
Jonathan Newman <jonathan@overfoc.us>
2014-06-07 07:28:31 -0500

    abstracted out common build steps

--- gulpfile.js
+++ gulpfile.js
@@ -59,16 +59,16 @@ gulp.task('test_bare', ['build-all'], function() {
 // Building tasks
 gulp.task('build-all', ['build_all', 'build_minimal', 'build_bare']);
 
-gulp.task("build_all", function() {
+var buildFootwork = function(buildProfile) {
   return gulp
-    .src(["source/build-profile/all.js"])
+    .src(["source/build-profile/" + buildProfile + ".js"])
     .pipe(header(banner, {
       pkg: pkg
     }))
     .pipe(fileImports())
     .pipe(replace(/FOOTWORK_VERSION/g, pkg.version))
-    .pipe(rename("footwork-all.js"))
-    .pipe(size({ title: '[all] Unminified' }))
+    .pipe(rename("footwork-" + buildProfile + ".js"))
+    .pipe(size({ title: '[' + buildProfile + '] Unminified' }))
     .pipe(gulp.dest("dist/"))
     .pipe(uglify({
       compress: { negate_iife: false }
@@ -76,54 +76,20 @@ gulp.task("build_all", function() {
     .pipe(header(banner, {
       pkg: pkg
     }))
-    .pipe(rename("footwork-all.min.js"))
-    .pipe(size({ title: '[all] Minified' }))
-    .pipe(size({ title: '[all] Minified', gzip: true }))
+    .pipe(rename("footwork-" + buildProfile + ".min.js"))
+    .pipe(size({ title: '[' + buildProfile + '] Minified' }))
+    .pipe(size({ title: '[' + buildProfile + '] Minified', gzip: true }))
     .pipe(gulp.dest("dist/"));
+};
+
+gulp.task("build_all", function() {
+  return buildFootwork('all');
 });
 
 gulp.task("build_minimal", function() {
-  return gulp
-    .src(["source/build-profile/minimal.js"])
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
-    .pipe(fileImports())
-    .pipe(replace(/FOOTWORK_VERSION/g, pkg.version))
-    .pipe(rename("footwork-minimal.js"))
-    .pipe(size({ title: '[minimal] Unminified' }))
-    .pipe(gulp.dest("dist/"))
-    .pipe(uglify({
-      compress: { negate_iife: false }
-    }))
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
-    .pipe(rename("footwork-minimal.min.js"))
-    .pipe(size({ title: '[minimal] Minified' }))
-    .pipe(size({ title: '[minimal] Minified', gzip: true }))
-    .pipe(gulp.dest("dist/"));
+  return buildFootwork('minimal');
 });
 
 gulp.task("build_bare", function() {
-  return gulp
-    .src(["source/build-profile/bare.js"])
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
-    .pipe(fileImports())
-    .pipe(replace(/FOOTWORK_VERSION/g, pkg.version))
-    .pipe(rename("footwork-bare.js"))
-    .pipe(size({ title: '[bare] Unminified' }))
-    .pipe(gulp.dest("dist/"))
-    .pipe(uglify({
-      compress: { negate_iife: false }
-    }))
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
-    .pipe(rename("footwork-bare.min.js"))
-    .pipe(size({ title: '[bare] Minified' }))
-    .pipe(size({ title: '[bare] Minified', gzip: true }))
-    .pipe(gulp.dest("dist/"));
+  return buildFootwork('bare');
 });
