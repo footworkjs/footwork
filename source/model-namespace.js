15eb08eec6967e86763725fc4ea638437326e141
Jonathan Newman <jonathan@overfoc.us>
2014-06-10 07:24:09 -0500

    Bumped to 0.1.9, simplified and fixed bug related to model namespace registration at creation. Updated docs and added more/better annotations, updated docs styles as well.

--- source/model-namespace.js
+++ source/model-namespace.js
@@ -1,6 +1,10 @@
 // model-namespace.js
 // ------------------
 
+// Prepare an empty namespace stack.
+// This is where footwork registers its current working namespace name. Each new namespace is
+// 'unshifted' and 'shifted' as they are entered and exited, keeping the most current at
+// index 0.
 ko.__nsStack = [];
 
 // Creates and returns a new namespace channel
@@ -54,6 +58,7 @@ ko.getModels = function(namespaceName) {
 
 // Tell footwork whether or not it should count and keep references to all created models
 // NOTE: This can lead to memory leaks and should not be used in production.
+var debugModels = false;
 ko.debugModels = function(state) {
   debugModels = state;
 };
@@ -63,12 +68,17 @@ ko.refreshModels = function() {
   _.invoke(ko.getModels(), 'refreshReceived');
 };
 
-var models = {},
-    modelNum = 0,
-    debugModels = false;
+// Initialize the models registry
+var models = {};
 
+// Counter used in the event no namespace is provided for a model
+var modelNum = 0;
+
+// This counter is used when model options { autoIncrement: true } and more than one model
+// having the same namespace is instantiated. This is used in the event you do not want
+// multiple copies of the same model to share the same namespace (if they do share a
+// namespace, they receive all of the same events/messages/commands/etc).
 var namespaceNameCounter = {};
-
 var indexedNamespaceName = function(name, autoIncrement) {
   if(namespaceNameCounter[name] === undefined) {
     namespaceNameCounter[name] = 0;
@@ -93,17 +103,14 @@ ko.model = function(modelOptions) {
       modelOptions.namespace = indexedNamespaceName(modelOptions.namespace, modelOptions.autoIncrement);
 
       this._modelOptions = modelOptions;
-      ko.enterNamespaceName(modelOptions.namespace);
 
       this._options = _.extend({
         namespace: modelOptions.namespace || ('namespace' + modelNum)
       }, options);
       modelNum++;
 
-      this.namespace = ko.namespace( this.namespace = this._options.namespace );
-      if(ko.currentNamespace().channel !== '/') {
-        this.namespace = ko.currentNamespace();
-      }
+      ko.enterNamespaceName( this._options.namespace );
+      this.namespace = ko.currentNamespace();
       this._globalChannel = ko.namespace();
     },
     mixin: {
