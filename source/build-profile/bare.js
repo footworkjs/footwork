c42c9b4c4bc9be3126446bb19bc5ac922fdb316f
Jonathan Newman <jonathan@overfoc.us>
2014-06-22 09:17:53 -0500

    added Qajax as ajax library dependency, updated which-to-use.txt

--- source/build-profile/bare.js
+++ source/build-profile/bare.js
@@ -1,13 +1,12 @@
 (function (root, factory) {
   if (typeof define === 'function' && define.amd) {
-    define(['lodash', 'knockout', 'postal', 'Q', 'delegate', 'Apollo'], factory);
+    define(['lodash', 'knockout', 'postal', 'q', 'delegate', 'Apollo', 'Qajax'], factory);
   } else if (typeof exports === 'object') {
-    module.exports = factory(require('lodash'), require('knockout'), require('postal'), require('Q'), require('delegate'), require('Apollo'));
+    module.exports = factory(require('lodash'), require('knockout'), require('postal'), require('q'), require('delegate'), require('Apollo'), require('Qajax'));
   } else {
-    root.ko = factory(_, ko, postal, Q, delegate, Apollo);
+    root.ko = factory(_, ko, postal, Q, delegate, Apollo, Qajax);
   }
-}(this, function (_, ko, postal, Q, delegate, Apollo) {
-  var definedModules = [ '_', 'ko', 'postal', 'Q', 'delegate', 'Apollo' ];
+}(this, function (_, ko, postal, Q, delegate, Apollo, Qajax) {
   var windowObject = window;
   
   return (function() {
@@ -18,7 +17,8 @@
       postal: postal,
       delegate: delegate,
       Apollo: Apollo,
-      Q: Q
+      Q: Q,
+      Qajax: Qajax
     });
 
     /**
@@ -33,9 +33,9 @@
     // list of dependencies to 'export' inside the library as .embed properties
     var embeddedDependencies = [ 'riveter' ];
 
-    return (function footwork(embedded, _, ko, postal, Apollo, riveter, delegate, Q) {
+    return (function footwork(embedded, _, ko, postal, Apollo, riveter, delegate, Q, Qajax) {
       //import("../main.js");
       return ko;
-    })( root._.pick(root, embeddedDependencies), root._, root.ko, root.postal, root.Apollo, root.riveter, root.delegate, root.Q );
+    })( root._.pick(root, embeddedDependencies), root._, root.ko, root.postal, root.Apollo, root.riveter, root.delegate, root.Q, root.Qajax );
   })();
 }));
