e4eeb10df896c27923024b0e5abc89827b9c6ba1
Jonathan Newman <jonathan@overfoc.us>
2014-06-21 19:36:46 -0500

    more WIP router/etc...added event delegation library for <a> clicks

--- source/router.js
+++ source/router.js
@@ -1,49 +1,4 @@
-/**
-  {
-    routes: [{
-      route: 'test/route(/:optional)',
-      title: function() {
-        return ko.request('nameSpace', 'broadcast:someVariable');
-      },
-      nav: true
-    }]
-  }
- */
-var routes = [];
-var navigationModel;
-
-var router = ko.router = function(options) {
-  var router = this.router;
-
-  if(_.isObject(options)) {
-    _.map(options.routes, function(route) {
-      router.add(route);
-    });
-  }
-
-  return router;
-};
-
-router.add = function(route) {
-  routes.push(route);
-  if( _.has(navigationModel, 'valueHasMutated') ) {
-    navigationModel.valueHasMutated();
-  }
-  console.log('router.add', route);
-};
-
-router.navigationModel = function() {
-  if(typeof navigationModel === 'undefined') {
-    navigationModel = ko.observableArray( routes );
-  }
-  return navigationModel;
-};
-
-router.activate = _.once( _.bind(function() {
-  
-}, router) );
-
-// Note, URL parsing/parametrization code shamelessly 'borrowed' from Durandal (http://durandaljs.com/) - Thanks!
+// Sourced: https://github.com/BlueSpire/Durandal/blob/e88fd385fb930d38456e35812b44ecd6ea7d8f4c/platforms/Bower/Durandal/js/plugins/router.js
 var optionalParam = /\((.*?)\)/g;
 var namedParam = /(\(\?)?:\w+/g;
 var splatParam = /\*\w+/g;
@@ -60,3 +15,74 @@ function routeStringToRegExp(routeString) {
 
   return new RegExp('^' + routeString + '$', routesAreCaseSensitive ? undefined : 'i');
 }
+
+/**
+  {
+    routes: [{
+      route: 'test/route(/:optional)',
+      title: function() {
+        return ko.request('nameSpace', 'broadcast:someVariable');
+      },
+      nav: true
+    }]
+  }
+ */
+var routes = [];
+var navigationModel;
+
+var router = ko.router = function(config) {
+  var router = this.router;
+
+  router.config = config;
+
+  if(_.isObject(config)) {
+    _.map(config.routes, function(route) {
+      router.add(route);
+    });
+  }
+
+  return router.activate();
+};
+
+router.config = {};
+router.namespace = ko.namespace('router');
+
+router.add = function(route) {
+  routes.push(route);
+
+  if( _(route).has('nav') && route.nav === true && _(navigationModel).has('valueHasMutated') ) {
+    navigationModel.push(route);
+  }
+  console.log('router.add', route);
+};
+
+router.navigationModel = function() {
+  if(typeof navigationModel === 'undefined') {
+    navigationModel = ko.observableArray( _(routes).where({ nav: true }) );
+  }
+  return navigationModel;
+};
+
+router.stateChanged = function(url) {
+  url = url || History.getState().url;
+  this.namespace.publish('stateChanged', url);
+  var route = this.getRouteFor(url);
+};
+
+router.getRouteFor = function(url) {
+  var route = router.config.route404;
+  _.each(routes, function(route) {
+    console.log(route);
+  });
+};
+
+router.activate = _.once( _.bind(function() {
+  console.log('router.activate');
+  delegate(document).on('click', 'a', function(event) {
+    console.info('delegateClick-event', event.delegateTarget);
+  });
+
+  History.Adapter.bind( window, 'statechange', router.stateChanged);
+  router.stateChanged();
+  return router;
+}, router) );
