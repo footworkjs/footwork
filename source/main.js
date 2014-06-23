d286dcc5a233bae4d2362adaba92f0aded490a4f
Jonathan Newman <jonathan@overfoc.us>
2014-06-23 15:41:04 -0500

    working internal logger

--- source/main.js
+++ source/main.js
@@ -14,91 +14,25 @@ if (!String.prototype.trim) {
   };
 }
 
-//see http://patik.com/blog/complete-cross-browser-console-log/
-// Tell IE9 to use its built-in console
-var treatAsIE8 = false;
-if (Function.prototype.bind && (typeof console === 'object' || typeof console === 'function') && typeof console.log == 'object') {
-  try {
-    ['log', 'info', 'warn', 'error', 'assert', 'dir', 'clear', 'profile', 'profileEnd']
-      .forEach(function(method) {
-        console[method] = this.call(console[method], console);
-      }, Function.prototype.bind);
-  } catch (ex) {
-    treatAsIE8 = true;
-  }
-}
-
 // misc utility noop function
 var noop = function() { };
 
 // Initialize the debugLevel observable, this controls
 // what level of debug statements are logged to the console
 // 0 === off
-// 1 === errors
+// 1 === errors / problems only
 // 2 === notices (very noisy)
-ko.debugLevel = ko.observable(0);
+ko.debugLevel = ko.observable(1);
 
-// internal logging method used when debugging is on
-var log;
-ko.log = log = function() {
-  if(ko.debugLevel() > 2) {
-    // originally sourced from Durandal (http://durandaljs.com/)
-    try {
-      // Modern browsers
-      if (typeof console != 'undefined' && typeof console.log == 'function') {
-        // Opera 11
-        if (window.opera) {
-          var i = 0;
-          while (i < arguments.length) {
-            console.log('Item ' + (i + 1) + ': ' + arguments[i]);
-            i++;
-          }
-        }
-        // All other modern browsers
-        else if ((slice.call(arguments)).length == 1 && typeof slice.call(arguments)[0] == 'string') {
-          console.log((slice.call(arguments)).toString());
-        } else {
-          console.log.apply(console, slice.call(arguments));
-        }
-      }
-      // IE8
-      else if ((!Function.prototype.bind || treatAsIE8) && typeof console != 'undefined' && typeof console.log == 'object') {
-        Function.prototype.call.call(console.log, console, slice.call(arguments));
-      }
-
-      // IE7 and lower, and other old browsers
-    } catch (ignore) { }
+// expose internal logging methods
+ko.log = function() {
+  if(ko.debugLevel() >= 2) {
+    log.apply(null, arguments);
   }
 };
-
-var logError;
-ko.logError = logError = function(error, err) {
-  if(ko.debugLevel() > 1) {
-    // originally sourced from Durandal (http://durandaljs.com/)
-    var exception;
-    
-    if(error instanceof Error){
-      exception = error;
-    } else {
-      exception = new Error(error);
-    }
-    
-    exception.innerError = err;
-    
-    //Report the error as an error, not as a log
-    try {
-      // Modern browsers (it's only a single item, no need for argument splitting as in log() above)
-      if (typeof console != 'undefined' && typeof console.error == 'function') {
-        console.error(exception);
-      }
-      // IE8
-      else if ((!Function.prototype.bind || treatAsIE8) && typeof console != 'undefined' && typeof console.error == 'object') {
-        Function.prototype.call.call(console.error, console, exception);
-      }
-      // IE7 and lower, and other old browsers
-    } catch (ignore) { }
-
-    throw exception;
+ko.logError = function() {
+  if(ko.debugLevel() >= 1) {
+    log.apply(null, arguments);
   }
 };
 
