c3c1f1e357e37b93867af93fcf5f8ddc08d9d191
Jonathan Newman <jonathan@overfoc.us>
2014-07-20 22:59:47 -0500

    added tagIsComponent(), renamed scripts attribute to viewModels in component location hash object, changed namespace.getNamespaceName() to namespace.getName()

--- dist/footwork-bare.js
+++ dist/footwork-bare.js
@@ -516,7 +516,7 @@ var makeNamespace = ko.namespace = function(namespaceName, $parentNamespace) {
   namespace.event.handler = _.bind( registerNamespaceEventHandler, namespace );
   namespace.event.handler.unregister = _.bind( unregisterNamespaceEventHandler, namespace );
 
-  namespace.getNamespaceName = function() {
+  namespace.getName = function() {
     return this.channel;
   };
   return namespace;
@@ -563,7 +563,7 @@ viewModelMixins.push({
   },
   mixin: {
     getNamespaceName: function() {
-      return this.$namespace.getNamespaceName();
+      return this.$namespace.getName();
     },
     broadcastAll: function() {
       var model = this;
@@ -747,6 +747,25 @@ var normalTags = [
   'span', 'strike', 'strong', 'style', 'sub', 'summary', 'sup', 'svg', 'table', 'tbody', 'td', 'template', 'textarea',
   'tfoot', 'th', 'thead', 'time', 'title', 'tr', 'track', 'u', 'ul', 'var', 'video', 'wbr', 'xmp'
 ];
+ko.components.tagIsComponent = function(tagName, isComponent) {
+  isComponent = (typeof isComponent === 'undefined' ? true : isComponent);
+
+  if( _.isArray(tagName) === true ) {
+    _.each(tagName, function(tag) {
+      ko.components.tagIsComponent(tag, isComponent);
+    });
+  }
+
+  if(isComponent !== true) {
+    if( _.contains(normalTags, tagName) === false ) {
+      normalTags.push(tagName);
+    }
+  } else {
+    normalTags = _.filter(normalTags, function(normalTagName) {
+      return normalTagName !== tagName;
+    });
+  }
+};
 
 ko.components.getComponentNameForNode = function(node) {
   var tagName = node.tagName && node.tagName.toLowerCase();
@@ -759,7 +778,7 @@ ko.components.getComponentNameForNode = function(node) {
 
 var defaultComponentLocation = {
   combined: null,
-  scripts: '/components',
+  viewModels: '/components',
   templates: '/components'
 };
 var registerComponentLocation = ko.components.loadRelativeTo = function(rootURL, returnTheValue) {
@@ -768,12 +787,12 @@ var registerComponentLocation = ko.components.loadRelativeTo = function(rootURL,
     componentLocation = _.extend({}, defaultComponentLocation);
   }
 
-  if( _.isObject(rootURL) === true && typeof rootURL.scripts !== 'undefined' && typeof rootURL.templates !== 'undefined' ) {
+  if( _.isObject(rootURL) === true && typeof rootURL.viewModels !== 'undefined' && typeof rootURL.templates !== 'undefined' ) {
     componentLocation = rootURL;
   } else if( typeof rootURL === 'string' ) {
     componentLocation = {
       combined: rootURL,
-      scripts: null,
+      viewModels: null,
       templates: null
     };
   }
@@ -805,7 +824,7 @@ var footworkDefaultLoader = ko.components.footworkDefaultLoader = {
         configOptions = { require: componentLocation.combined + '/' + jsFile };
       } else {
         configOptions = {
-          viewModel: { require: (componentLocation.scripts || componentLocation.script) + '/' + jsFile },
+          viewModel: { require: (componentLocation.viewModels || componentLocation.viewModel) + '/' + jsFile },
           template: { require: 'text!' + (componentLocation.templates || componentLocation.template) + '/' + templateFile }
         };
       }
