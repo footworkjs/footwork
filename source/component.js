08c021d2d9d6c832bd51a0db54017378da57010a
Jonathan Newman <jonathan@overfoc.us>
2014-07-26 18:49:25 -0500

    reorg, split off resource resolution utility code out of component.js and into separate file

--- source/component.js
+++ source/component.js
@@ -59,114 +59,6 @@ ko.components.tagIsComponent = function(tagName, isComponent) {
   }
 };
 
-var resourceFileExtensions = {
-  combined: '.js',
-  viewModel: '.js',
-  template: '.html'
-};
-
-ko.components.setFileExtensions = function(fileType, extension) {
-  if( typeof fileType === 'object' ) {
-    _.extend(resourceFileExtensions, fileType);
-  } else if(typeof resourceFileExtensions[fileType] !== 'undefined') {
-    resourceFileExtensions[fileType] = extension;
-  }
-};
-
-ko.components.getFileExtensions = function() {
-  return _.clone(resourceFileExtensions);
-};
-
-ko.components.getNormalTagList = function() {
-  return normalTags.splice(0);
-};
-
-ko.components.getComponentNameForNode = function(node) {
-  var tagName = node.tagName && node.tagName.toLowerCase();
-
-  if( ko.components.isRegistered(tagName) || _.indexOf(normalTags, tagName) === -1 ) {
-    return tagName;
-  }
-  return null;
-};
-
-var defaultResourceLocation = {
-  combined: null,
-  viewModels: '/viewModel/',
-  templates: '/component/'
-};
-var resourceRelativeLocation = function(rootURL, returnTheValue) {
-  var componentLocation = defaultResourceLocation;
-  if(returnTheValue === true) {
-    componentLocation = _.extend({}, defaultResourceLocation);
-  }
-
-  if( _.isObject(rootURL) === true ) {
-    // assume some combination of defaultResourceLocation and normalize the parameters
-    _.extend(componentLocation, _.reduce(rootURL, function(options, paramValue, paramName) {
-      if(paramName === 'viewModel') {
-        options.viewModels = paramValue;
-        delete options.viewModel;
-      } else if(paramName === 'template') {
-        options.templates = paramValue;
-        delete options.template;
-      } else {
-        options[paramName] = paramValue;
-      }
-      return options;
-    }, {}));
-  } else if( typeof rootURL === 'string' ) {
-    componentLocation = {
-      combined: rootURL,
-      viewModels: null,
-      templates: null
-    };
-  }
-
-  if(returnTheValue === true) {
-    return componentLocation;
-  } else {
-    defaultResourceLocation = componentLocation;
-  }
-};
-
-var componentRelativeLocation = ko.components.loadRelativeTo = function(rootURL, returnTheValue) {
-  var returnValue = resourceRelativeLocation(rootURL, returnTheValue);
-  if(returnTheValue === true) {
-    return returnValue;
-  }
-};
-
-var resourceLocations = ko.resourceLocations = {};
-var registerLocationOfComponent = ko.components.registerLocationOf = function(componentName, componentLocation) {
-  if( _.isArray(componentName) === true ) {
-    _.each(componentName, function(name) {
-      registerLocationOfComponent(name, componentLocation);
-    });
-  }
-  resourceLocations[ componentName ] = componentRelativeLocation(componentLocation, true);
-};
-
-var viewModelRelativeLocation = ko.viewModel.loadRelativeTo = function(rootURL, returnTheValue) {
-  var returnValue = resourceRelativeLocation({ viewModel: rootURL }, returnTheValue);
-  if(returnTheValue === true) {
-    return returnValue;
-  }
-};
-
-var registerLocationOfViewModel = ko.viewModel.registerLocationOf = function(viewModelName, viewModelLocation) {
-  if( _.isArray(viewModelName) === true ) {
-    _.each(viewModelName, function(name) {
-      registerLocationOfViewModel(name, viewModelLocation);
-    });
-  }
-  resourceLocations[ viewModelName ] = viewModelRelativeLocation(viewModelLocation, true);
-};
-
-// Return the resource definition for the supplied resourceName
-var getResourceLocation = ko.getResourceLocation = function(resourceName) {
-  return resourceLocations[resourceName] || defaultResourceLocation;
-};
 
 // Monkey patch for knockout, enables the viewModel 'component' to initialize a model and bind to the html as intended
 // TODO: Do this differently once this is resolved: https://github.com/knockout/knockout/issues/1463
@@ -281,17 +173,15 @@ ko.components.loaders.push({
 });
 
 // Use the component update binding to provide the afterBinding() lifecycle event
-_.extend(ko.bindingHandlers.component, {
-  update: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
-    if( isViewModel(viewModel) === true ) {
-      var configParams = viewModel.__getConfigParams();
-      if( _.isFunction(configParams.afterInsert) === true && typeof configParams.afterInsert.wasCalled === 'undefined' ) {
-        configParams.afterBinding.wasCalled = true;
-        configParams.afterBinding.call(viewModel, element, valueAccessor, allBindings, viewModel, bindingContext);
-      }
+ko.bindingHandlers.component.update = function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+  if( isViewModel(viewModel) === true ) {
+    var configParams = viewModel.__getConfigParams();
+    if( _.isFunction(configParams.afterInsert) === true && typeof configParams.afterInsert.wasCalled === 'undefined' ) {
+      configParams.afterBinding.wasCalled = true;
+      configParams.afterBinding.call(viewModel, element, valueAccessor, allBindings, viewModel, bindingContext);
     }
   }
-});
+};
 
 // outlets can only exist within parent components
 ko.components.register('outlet', {
@@ -312,8 +202,6 @@ ko.components.register('outlet', {
       this.errors.push('Could not locate outlet observable ($parentViewModel.' + this.outletName + 'Outlet' + ' is undefined).');
     }
   },
-  // use comment bindings!
-  // template: '<!-- ko if: outletIsActive -->[OUTLET]<div data-bind="component: { name: targetComponent, params: { parentNamespace: namespace, $outletViewModel: $data, $parentViewModel: $parent } }, class: outletName"></div><!-- /ko -->'
   template: '\
     <!-- ko if: outletIsActive -->\
       <!-- ko component: { name: targetComponent, params: { errors: errors } } --><!-- /ko -->\
