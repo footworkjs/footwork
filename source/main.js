b2d9dbe596641932b5f24dac645f82090da428dd
Jonathan Newman <jonathan@overfoc.us>
2014-08-22 21:21:22 -0500

    reorganization / cleanup, trimmed down main.js

--- source/main.js
+++ source/main.js
@@ -7,14 +7,17 @@ ko._footworkVersion = 'FOOTWORK_VERSION';
 // Expose any embedded dependencies
 ko.embed = embedded;
 
+// misc regex patterns
+var hasTrailingSlash = /\/$/i;
+var endsInDotJS = /\.js$/;
+
 // misc utility functions
 var noop = function() { };
 
 var isObservable = ko.isObservable;
 
-var trailingSlash = /\/$/i;
 var isPath = function(pathOrLocation) {
-  return trailingSlash.test(pathOrLocation) === true;
+  return hasTrailingSlash.test(pathOrLocation) === true;
 };
 
 var isFunction = _.isFunction;
@@ -26,44 +29,15 @@ var isUndefined = _.isUndefined;
 var isArray = _.isArray;
 var isNull = _.isNull;
 
+// Registry which stores the mixins that are automatically added to each viewModel
+var viewModelMixins = [];
+
 // Initialize the debugLevel observable, this controls
 // what level of debug statements are logged to the console
 // 0 === off
 // 1 === errors / problems only
 // 2 === notices (very noisy)
 ko.debugLevel = ko.observable(1);
-
-// Preserve the original applyBindings method for later use
-var originalApplyBindings = ko.applyBindings;
-
-// Override the original applyBindings method to provide and enable 'viewModel' life-cycle hooks/events.
-var doNotSetContextOnRouter = false;
-var setContextOnRouter = true;
-var applyBindings = ko.applyBindings = function(viewModel, element, shouldSetContext) {
-  originalApplyBindings(viewModel, element);
-  shouldSetContext = isUndefined(shouldSetContext) === true ? setContextOnRouter : shouldSetContext;
-
-  if( isViewModel(viewModel) === true ) {
-    var $configParams = viewModel.__getConfigParams();
-    
-    if( isFunction($configParams.afterBinding) === true ) {
-      $configParams.afterBinding.call(viewModel, element);
-    }
-
-    if( shouldSetContext === setContextOnRouter && isRouter( viewModel.$router ) === true ) {
-      viewModel.$router.context( ko.contextFor(element) );
-    }
-    
-    if( isUndefined(element) === false ) {
-      ko.utils.domNodeDisposal.addDisposeCallback(element, function() {
-        viewModel.__shutdown();
-      });
-    }
-  }
-};
-
-// This stores the mixins which are automatically added to each viewModel
-var viewModelMixins = [];
 
 //import("namespace.js");
 //import("broadcast-receive.js");
