694813da8719d725fe5bf9d2fd11f12fd090641a
Jonathan Newman <jonathan@overfoc.us>
2014-08-31 09:12:02 -0500

    added ko.viewModels resource capability, split off from component resource definitions, misc api cleanup

--- dist/footwork-all.js
+++ dist/footwork-all.js
@@ -10471,11 +10471,10 @@ ko.bindingHandlers.component.init = function(element, valueAccessor, allBindings
     var tagName = element.tagName.toLowerCase();
     if( tagName === 'viewmodel' ) {
       var values = valueAccessor();
-      var name = element.getAttribute('module') || element.getAttribute('data-module');
+      var viewModelName = ( !isUndefined(values.params) ? ko.unwrap(values.params.name) : undefined ) || element.getAttribute('module') || element.getAttribute('data-module');
 
-      if( !isUndefined(name) ) {
-        var viewModelName = ko.unwrap(values.params.name);
-        var resourceLocation = getResourceLocation( viewModelName ).viewModels;
+      if( !isUndefined(viewModelName) ) {
+        var resourceLocation = getViewModelResourceLocation( viewModelName );
 
         if( isFunction(require) && isFunction(require.defined) && require.defined(viewModelName) ) {
           // we have found a matching resource that is already cached by require, lets use it
@@ -10506,7 +10505,7 @@ ko.bindingHandlers.component.init = function(element, valueAccessor, allBindings
         if( isString(resourceLocation) ) {
           if( isFunction(require) ) {
             if( isPath(resourceLocation) ) {
-              resourceLocation = resourceLocation + name;
+              resourceLocation = resourceLocation + viewModelName;
             }
             if( resourceLocation !== viewModelName && endsInDotJS.test(resourceLocation) === false ) {
               resourceLocation = resourceLocation + resourceFileExtensions.viewModel;
@@ -10544,104 +10543,6 @@ ko.bindingHandlers.component.init = function(element, valueAccessor, allBindings
   }
 
   return originalComponentInit(element, theValueAccessor, allBindings, viewModel, bindingContext);
-};
-// resource.js
-// ------------------
-
-var resourceFileExtensions = {
-  combined: '.js',
-  viewModel: '.js',
-  template: '.html'
-};
-
-ko.components.setFileExtensions = function(fileType, extension) {
-  if( isObject(fileType) ) {
-    extend(resourceFileExtensions, fileType);
-  } else if( !isUndefined(resourceFileExtensions[fileType]) ) {
-    resourceFileExtensions[fileType] = extension;
-  }
-};
-
-ko.components.getFileExtensions = function() {
-  return clone(resourceFileExtensions);
-};
-
-ko.components.getNormalTagList = function() {
-  return normalTags.splice(0);
-};
-
-ko.components.getComponentNameForNode = function(node) {
-  var tagName = isString(node.tagName) && node.tagName.toLowerCase();
-
-  if( ko.components.isRegistered(tagName) || indexOf(normalTags, tagName) === -1 ) {
-    return tagName;
-  }
-  return null;
-};
-
-var defaultResourceLocation = {
-  combined: null,
-  viewModels: '/viewModel/',
-  templates: '/component/'
-};
-var resourceRelativeLocation = function(rootURL, updateConfig) {
-  var componentLocation = (isUndefined(updateConfig) || updateConfig === true) ? defaultResourceLocation : clone(defaultResourceLocation);
-
-  if( isObject(rootURL) ) {
-    // assume some combination of defaultResourceLocation and normalize the parameters
-    extend(componentLocation, reduce(rootURL, function(options, paramValue, paramName) {
-      if(paramName === 'viewModel') {
-        options.viewModels = paramValue;
-        delete options.viewModel;
-      } else if(paramName === 'template') {
-        options.templates = paramValue;
-        delete options.template;
-      } else {
-        options[paramName] = paramValue;
-      }
-      return options;
-    }, {}));
-  } else if( isString(rootURL) ) {
-    componentLocation = {
-      combined: rootURL,
-      viewModels: null,
-      templates: null
-    };
-  }
-
-  return componentLocation;
-};
-
-var componentRelativeLocation = ko.components.loadRelativeTo = function(locations, updateConfig) {
-  return resourceRelativeLocation(locations, updateConfig);
-};
-
-var resourceLocations = ko.resourceLocations = {};
-var registerLocationOfComponent = ko.components.registerLocationOf = function(componentName, componentLocation) {
-  if( isArray(componentName) ) {
-    each(componentName, function(name) {
-      registerLocationOfComponent(name, componentLocation);
-    });
-  }
-  resourceLocations[ componentName ] = componentRelativeLocation(componentLocation, false);
-};
-
-var viewModelRelativeLocation = ko.viewModel.loadRelativeTo = function(rootURL, updateConfig) {
-  return resourceRelativeLocation({ viewModel: rootURL }, updateConfig);
-};
-
-var registerLocationOfViewModel = ko.viewModel.registerLocationOf = function(viewModelName, viewModelLocation) {
-  if( isArray(viewModelName) ) {
-    each(viewModelName, function(name) {
-      registerLocationOfViewModel(name, viewModelLocation);
-    });
-  }
-  resourceLocations[ viewModelName ] = viewModelRelativeLocation(viewModelLocation, false);
-};
-
-// Return the resource definition for the supplied resourceName
-var getResourceLocation = ko.getResourceLocation = function(resourceName) {
-  return resourceLocations[resourceName] || defaultResourceLocation;
 };
 // component.js
 // ------------------
@@ -10797,7 +10698,7 @@ ko.components.loaders.push( ko.components.requireLoader = {
     var combinedFile = componentName + resourceFileExtensions.combined;
     var viewModelFile = componentName + resourceFileExtensions.viewModel;
     var templateFile = componentName + resourceFileExtensions.template;
-    var componentLocation = getResourceLocation(componentName);
+    var componentLocation = getComponentResourceLocation(componentName);
     var configOptions = null;
     var viewModelPath;
     var templatePath;
@@ -10890,6 +10791,123 @@ ko.components.register('error', {
       </div>\
     </div>'
 });
+// resource.js
+// ------------------
+
+var resourceFileExtensions = {
+  combined: '.js',
+  viewModel: '.js',
+  template: '.html'
+};
+
+ko.components.setFileExtensions = function(fileType, extension) {
+  if( isObject(fileType) ) {
+    extend(resourceFileExtensions, fileType);
+  } else if( !isUndefined(resourceFileExtensions[fileType]) ) {
+    resourceFileExtensions[fileType] = extension;
+  }
+};
+
+ko.components.getFileExtensions = function() {
+  return clone(resourceFileExtensions);
+};
+
+ko.components.getNormalTagList = function() {
+  return normalTags.splice(0);
+};
+
+ko.components.getComponentNameForNode = function(node) {
+  var tagName = isString(node.tagName) && node.tagName.toLowerCase();
+
+  if( ko.components.isRegistered(tagName) || indexOf(normalTags, tagName) === -1 ) {
+    return tagName;
+  }
+  return null;
+};
+
+var defaultComponentLocation = {
+  combined: null,
+  viewModels: '/viewModel/',
+  templates: '/component/'
+};
+var componentRelativeLocation = ko.components.loadRelativeTo = function(root, updateDefault) {
+  var componentLocation = (isUndefined(updateDefault) || updateDefault === true) ? defaultComponentLocation : clone(defaultComponentLocation);
+
+  if( isObject(root) ) {
+    // assume some combination of defaultComponentLocation and normalize the parameters
+    extend(componentLocation, reduce(root, function(options, paramValue, paramName) {
+      if(paramName === 'viewModel') {
+        options.viewModels = paramValue;
+        delete options.viewModel;
+      } else if(paramName === 'template') {
+        options.templates = paramValue;
+        delete options.template;
+      } else {
+        options[paramName] = paramValue;
+      }
+      return options;
+    }, {}));
+  } else if( isString(root) ) {
+    componentLocation = {
+      combined: rootURL,
+      viewModels: null,
+      templates: null
+    };
+  }
+
+  return componentLocation;
+};
+
+var componentResourceLocations = ko.components.resourceLocations = {};
+var registerLocationOfComponent = ko.components.registerLocationOf = function(componentName, componentLocation) {
+  if( isArray(componentName) ) {
+    each(componentName, function(name) {
+      registerLocationOfComponent(name, componentLocation);
+    });
+  }
+  componentResourceLocations[ componentName ] = componentRelativeLocation(componentLocation, false);
+};
+
+// Return the component resource definition for the supplied componentName
+var getComponentResourceLocation = ko.components.getResourceLocation = function(componentName) {
+  if( isUndefined(componentName) ) {
+    return componentResourceLocations;
+  }
+  return componentResourceLocations[componentName] || defaultComponentLocation;
+};
+
+var defaultViewModelLocation = '/viewModel/';
+var viewModelRelativeLocation = ko.viewModels.loadRelativeTo = function(root, updateDefault) {
+  var viewModelLocation = defaultViewModelLocation;
+
+  if( isString(root) ) {
+    viewModelLocation = root;
+  }
+
+  if(updateDefault) {
+    defaultViewModelLocation = viewModelLocation;
+  }
+
+  return viewModelLocation;
+};
+
+var viewModelResourceLocations = ko.components.resourceLocations = {};
+var registerLocationOfViewModel = ko.viewModels.registerLocationOf = function(viewModelName, viewModelLocation) {
+  if( isArray(viewModelName) ) {
+    each(viewModelName, function(name) {
+      registerLocationOfViewModel(name, viewModelLocation);
+    });
+  }
+  viewModelResourceLocations[ viewModelName ] = viewModelRelativeLocation(viewModelLocation, false);
+};
+
+// Return the viewModel resource definition for the supplied viewModelName
+var getViewModelResourceLocation = ko.viewModels.getResourceLocation = function(viewModelName) {
+  if( isUndefined(viewModelName) ) {
+    return viewModelResourceLocations;
+  }
+  return viewModelResourceLocations[viewModelName] || getComponentResourceLocation(viewModelName).viewModels || defaultViewModelLocation;
+};
 // extenders.js
 // ----------------
 
