32d3a26e36b5937e57bf6d24c260b93ffa0ed81f
Jonathan Newman <jonathan@overfoc.us>
2014-09-28 08:17:24 -0500

    Updated build process, created build-info.js, removed leftover template in docs

---
+++ docs/build-info.js
@@ -0,0 +1,7 @@
+/**
+ * Note: this file is generated via gulp, do not edit directly.
+ */
+window.footworkBuild = {
+  version: '0.2.0',
+  statement: 'A solid footing for knockout applications.'
+};
---
+++ docs/templates/build-info.js
@@ -0,0 +1,7 @@
+/**
+ * Note: this file is generated via gulp, do not edit directly.
+ */
+window.footworkBuild = {
+  version: 'FOOTWORK_VERSION',
+  statement: 'FOOTWORK_STATEMENT'
+};
