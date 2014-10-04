2ed701d1d055c105d82de37934800f4b49213f2f
Jonathan Newman <jonathan@overfoc.us>
2014-10-04 11:39:46 -0500

    undefinedRoute description now prioritized over inline supplied 'unknown: true' route config

--- source/router.js
+++ source/router.js
@@ -421,7 +421,7 @@ Router.prototype.normalizeURL = function(url) {
 };
 
 Router.prototype.getUnknownRoute = function() {
-  var unknownRoute = findWhere(this.getRouteDescriptions(), { unknown: true }) || null;
+  var unknownRoute = findWhere((this.getRouteDescriptions() || []).reverse(), { unknown: true }) || null;
 
   if( !isNull(unknownRoute) ) {
     unknownRoute = extend({}, baseRoute, {
