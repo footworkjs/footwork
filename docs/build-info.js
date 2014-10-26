a00f02525c190565dbc55d20cb84fc76980c8e90
Jonathan Newman <jonathan@overfoc.us>
2014-10-26 18:35:04 -0500

    added up-chevron to docs

--- docs/build-info.js
+++ docs/build-info.js
@@ -5,6 +5,6 @@
 window.footworkBuild = {
   version: '0.2.0',
   statement: 'A solid footing for web applications.',
-  timestamp: '2014-10-26T17:58:14-05:00',
+  timestamp: '2014-10-26T18:04:00-05:00',
   contributors: [{"name":"Jonathan Newman","email":"jonathan@staticty.pe","urls":[{"url":"https://github.com/reflectiveSingleton"},{"url":"http://staticty.pe"}]},{"name":"Travis Miller","email":"travis@travisamiller.com","urls":[{"url":"http://travisamiller.com"}]}]
 };
