44491dcc63b19e2b4d96b1081d320de622738307
Jonathan Newman <jonathan@overfoc.us>
2014-11-15 13:01:53 -0600

    added foot logo to main background, misc cleanup of docs

--- docs/scripts/app/router.js
+++ docs/scripts/app/router.js
@@ -1,4 +1,4 @@
-define([ "jquery", "footwork", "lodash", "highlight" ],
+define([ "jquery", "footwork", "lodash", "highlight", "jquery.collapsible" ],
   function( $, fw, _ ) {
     var $pageNamespace = fw.namespace('Page');
     var $pageSectionNamespace = fw.namespace('PageSection');
