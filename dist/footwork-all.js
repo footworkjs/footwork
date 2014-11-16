f2fd1696c1d68f101d63f8aa4af97b09ed050f92
Jonathan Newman <jonathan@overfoc.us>
2014-11-16 14:20:50 -0600

    more WIP viewModel-page.html, better hash handling by router

--- dist/footwork-all.js
+++ dist/footwork-all.js
@@ -10156,16 +10156,12 @@ fw.bindingHandlers.$route = {
               var hashIndex = currentRouteURL.indexOf('#');
               if( hashIndex === -1 ) {
                 hashIndex = currentRouteURL.length;
-              } else {
-                // This is a really ugly way of handling the hash.
-                // TODO: Need to figure out clean way of detecting when all components have been loaded, or 
-                // allow the user to inform the framework when loading is 'done', then trigger the hash change.
-                // For now, just a nasty setTimeout with a 500ms delay.
-                setTimeout(function() {
-                  windowObject.location.hash = currentRouteURL.substring(hashIndex);
-                }, 500);
               }
               $myRouter.setState( currentRouteURL.substring(0, hashIndex) );
+              
+              if(hashIndex !== currentRouteURL.length) {
+                windowObject.location.hash = currentRouteURL.substring(hashIndex);
+              }
             }
           }
         });
@@ -10211,7 +10207,7 @@ var Router = function( routerConfig, $viewModel, $context ) {
   }, this);
   
   this.currentRoute = fw.computed(function() {
-    return this.getRouteForURL( this.currentState() );
+    return this.getRouteForURL( this.normalizeURL(this.currentState()) );
   }, this);
   
   this.path = fw.computed(function() {
