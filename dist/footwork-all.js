c410618343faccaf7ea37cee99d59ba03360827d
Jonathan Newman <jonathan@overfoc.us>
2014-12-02 05:08:52 -0600

    updated version to pre-release 0.8.0, finalized docs layout for publishing

--- dist/footwork-all.js
+++ dist/footwork-all.js
@@ -1,7 +1,7 @@
 /**
  * footwork.js - A solid footing for web applications.
  * Author: Jonathan Newman (http://staticty.pe)
- * Version: v0.2.0-all
+ * Version: v0.8.0-pre-all
  * Url: http://footworkjs.com
  * License(s): MIT
  */
@@ -9372,7 +9372,7 @@ b,e);e.appendTo(v.createElement("div"));w.fragments={};return e};this.createJava
 var fw = ko;
 
 // Record the footwork version as of this build.
-fw.footworkVersion = '0.2.0';
+fw.footworkVersion = '0.8.0-pre';
 
 // Expose any embedded dependencies
 fw.embed = embedded;
