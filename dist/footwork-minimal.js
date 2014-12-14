397a7fc2370aa47983f23abb1092d31571ba50f4
Jonathan Newman <jonathan@overfoc.us>
2014-12-14 17:43:41 -0600

    more WIP on routing, fixed build error

--- dist/footwork-minimal.js
+++ dist/footwork-minimal.js
@@ -4868,7 +4868,7 @@ var isFullURL = fw.isFullURL = function(thing) {
 };
 
 var hasHTML5History = windowObject.history && windowObject.history.pushState;
-if(isObject(windowObject.History.options) && windowObject.History.options.html4Mode) {
+if(!isUndefined(windowObject.History) && isObject(windowObject.History.options) && windowObject.History.options.html4Mode) {
   // user is overriding to force html4mode hash-based history
   hasHTML5History = false;
 }
