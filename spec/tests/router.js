668aff73947f0c219319eddf4076890c78e016cc
Jonathan Newman <jonathan@overfoc.us>
2015-03-19 06:56:27 -0500

    more router tests

--- spec/tests/router.js
+++ spec/tests/router.js
@@ -370,4 +370,120 @@ describe('router', function () {
       }, 40);
     }, 40);
   });
+
+  it('can have callback triggered after outlet component is resolved and composed', function(done) {
+    var container = document.getElementById('outletCallback');
+    var controllerRan = false;
+    var componentInstantiated = false;
+    var outletCallbackRan = false;
+    var router;
+
+    fw.components.register('outletCallbackComponent', {
+      viewModel: function() {
+        componentInstantiated = true;
+      },
+      template: '<div class="outletCallbackComponent"></div>'
+    });
+
+    fw.router({
+      namespace: 'outletCallback',
+      autoRegister: true,
+      initialize: function() {
+        router = this;
+      },
+      routes: [
+        {
+          route: '/outletCallback',
+          controller: function() {
+            controllerRan = true;
+            this.$outlet('output', 'outletCallbackComponent', function(element) {
+              expect(element.tagName.toLowerCase()).to.be('outlet');
+              expect(element.children[0].className).to.be('outletCallbackComponent');
+              outletCallbackRan = true;
+            });
+          }
+        }
+      ]
+    });
+
+    expect(controllerRan).to.be(false);
+    expect(componentInstantiated).to.be(false);
+
+    fw.start(container);
+
+    setTimeout(function() {
+      router.setState('/outletCallback');
+      expect(controllerRan).to.be(true);
+
+      setTimeout(function() {
+        expect(componentInstantiated).to.be(true);
+        expect(outletCallbackRan).to.be(true);
+        done();
+      }, 40);
+    }, 40);
+  });
+
+  it('can instantiate and properly render an outlet after its router has initialized', function(done) {
+    var container = document.getElementById('outletAfterRouter');
+    var controllerRan = false;
+    var componentInstantiated = false;
+    var outletCallbackRan = false;
+    var router;
+    var viewModel;
+
+    fw.components.register('outletAfterRouter', {
+      viewModel: function() {
+        componentInstantiated = true;
+      },
+      template: '<div class="outletAfterRouter"></div>'
+    });
+
+    fw.viewModels.register('outletAfterRouter', fw.viewModel({
+      initialize: function() {
+        viewModel = this;
+        this.outletVisible = fw.observable(false);
+      }
+    }));
+
+    fw.router({
+      namespace: 'outletAfterRouter',
+      autoRegister: true,
+      initialize: function() {
+        router = this;
+      },
+      routes: [
+        {
+          route: '/outletAfterRouter',
+          controller: function() {
+            controllerRan = true;
+            this.$outlet('output', 'outletAfterRouter', function(element) {
+              expect(element.tagName.toLowerCase()).to.be('outlet');
+              expect(element.children[0].className).to.be('outletAfterRouter');
+              outletCallbackRan = true;
+            });
+          }
+        }
+      ]
+    });
+
+    expect(controllerRan).to.be(false);
+    expect(componentInstantiated).to.be(false);
+
+    fw.start(container);
+
+    setTimeout(function() {
+      router.setState('/outletAfterRouter');
+
+      expect(controllerRan).to.be(true);
+      expect(viewModel).to.be.an('object');
+
+      viewModel.outletVisible(true);
+
+      setTimeout(function() {
+        expect(componentInstantiated).to.be(true);
+        expect(outletCallbackRan).to.be(true);
+        done();
+      }, 40);
+    }, 40);
+  });
 });
