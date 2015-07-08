ed13c49084adb36dcb9178b3094fc484cc43c4a6
Jonathan Newman <jonathan@overfoc.us>
2015-07-08 07:38:51 -0500

    split apart dataModel entity logic
    Former-commit-id: eadf18085b5b377aec8db30af9995660a3436e13

---
+++ source/framework/entities/dataModel/mapTo.js
@@ -0,0 +1,59 @@
+// framework/entities/dataModel/mapTo.js
+// ------------------
+
+fw.subscribable.fn.mapTo = function(option) {
+  var mappedObservable = this;
+  var mapPath;
+  var dataModel;
+
+  if(isString(option)) {
+    mapPath = option;
+    dataModel = currentDataModelContext();
+  } else if(isObject(option)) {
+    mapPath = option.path;
+    dataModel = option.dataModel;
+  } else {
+    throw new Error('Invalid options supplied to mapTo');
+  }
+
+  if(!isDataModel(dataModel)) {
+    throw new Error('No dataModel context found/supplied for mapTo observable');
+  }
+
+  var mappings = dataModel.__mappings();
+  var primaryKey = getPrimaryKey(dataModel);
+  if( !isUndefined(mappings[mapPath]) && (mapPath !== primaryKey && dataModel.$id.__isOriginalPK)) {
+    throw new Error('the field \'' + mapPath + '\' is already mapped on this dataModel');
+  }
+
+  if(!isUndefined(mappings[mapPath]) && isFunction(mappings[mapPath].dispose)) {
+    // remapping a path, we need to dispose of the old one first
+    mappings[mapPath].dispose();
+  }
+
+  // add/set the registry entry for the mapped observable
+  mappings[mapPath] = mappedObservable;
+
+  if(mapPath === primaryKey) {
+    // mapping primary key, update/set the $id property on the dataModel
+    dataModel.$id = mappings[mapPath];
+  }
+
+  mappedObservable.isDirty = fw.observable(false);
+  var changeSubscription = mappedObservable.subscribe(function(value) {
+    dataModel.$namespace.publish('$.change', { param: mapPath, value: value });
+    mappedObservable.isDirty(true);
+  });
+
+  var disposeObservable = mappedObservable.dispose || noop;
+  if(isFunction(mappedObservable.dispose)) {
+    mappedObservable.dispose = function() {
+      changeSubscription.dispose();
+      disposeObservable.call(mappedObservable);
+    };
+  }
+
+  dataModel.__mappings.valueHasMutated();
+
+  return mappedObservable;
+};
--- source/framework/entities/dataModel/module.js
+++ source/framework/entities/dataModel/module.js
@@ -1,2 +1,12 @@
+//import("utility.js");
 //import("sync.js");
+//import("mapTo.js");
 //import("DataModel.js");
+
+runPostInit.push(function(runTask) {
+  fw.ajax = ajax;
+  extend(fw.settings, {
+    emulateHTTP: false,
+    emulateJSON: false
+  });
+});
