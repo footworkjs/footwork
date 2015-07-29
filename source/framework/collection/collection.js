2fdfe0fb594ba05bc9f892fc4932050d431e5eab
Jonathan Newman <jonathan@overfoc.us>
2015-07-29 09:06:28 -0500

    added disposal code for collections
    Former-commit-id: 68b72f8a754a7f3bd0b38f251ada465eb8cace50

--- source/framework/collection/collection.js
+++ source/framework/collection/collection.js
@@ -1,6 +1,12 @@
 // framework/collection/collection.js
 // ------------------
 
+function removeAndDispose(originalFunction) {
+  var removedItems = originalFunction.apply(this, arguments);
+  invoke(removedItems, 'dispose');
+  return removedItems;
+}
+
 fw.collection = function(conf) {
   return function initCollection(collectionData) {
     var collection = fw.observableArray();
@@ -13,7 +19,19 @@ fw.collection = function(conf) {
     var privateDataStore = {};
     collection.__private = privateData.bind(this, privateDataStore, config);
 
-    var originalRemove = this.remove;
+    // Make sure we dispose all dataModels when they are removed
+    collection.remove = removeAndDispose.bind(collection, collection.remove);
+    collection.pop = removeAndDispose.bind(collection, collection.pop);
+    collection.shift = removeAndDispose.bind(collection, collection.shift);
+    collection.splice = removeAndDispose.bind(collection, collection.splice);
+
+    collection.dispose = function() {
+      if(!collection.isDisposed) {
+        collection.isDisposed = true;
+        collection.$namespace.dispose();
+        invoke(collection(), 'dispose');
+      }
+    };
 
     extend(collection, collectionMethods, {
       $namespace: fw.namespace(config.namespace || uniqueId('collection')),
