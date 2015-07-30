cc9ba42655c436899a3a28c7f81e6617521ef0a5
Jonathan Newman <jonathan@overfoc.us>
2015-07-30 12:05:01 -0500

    reorg collection tests, more robust test for all necessary transforms in collection.$set()
    Former-commit-id: e809f01f9720e5fc1c157db42b9bfb80936b431b

--- spec/tests/collection.js
+++ spec/tests/collection.js
@@ -44,101 +44,6 @@ describe('collection', function () {
     expect(people()[1].email()).to.be(person2Data.email);
   });
 
-  it('can correctly trigger change/add/remove events for dataModels in a $set() call as appropriate', function() {
-    var peopleData = {
-      "person1Data": {
-        "id": 1,
-        "firstName": "Person1FirstNameTest",
-        "lastName": "Person1LastNameTest",
-        "email": "Person1EmailTest"
-      },
-      "person2Data": {
-        "id": 2,
-        "firstName": "Person2FirstNameTest",
-        "lastName": "Person2LastNameTest",
-        "email": "Person2EmailTest"
-      }
-    };
-
-    var Person = fw.dataModel({
-      namespace: 'PersonEventCheck',
-      initialize: function(person) {
-        person = person || {};
-        this.firstName = fw.observable(person.firstName || null).mapTo('firstName');
-        this.lastName = fw.observable(person.lastName || null).mapTo('lastName');
-        this.email = fw.observable(person.email || null).mapTo('email');
-      }
-    });
-
-
-    var unexpectedEvents = {};
-    var recordedEvents = {
-      'person1Data _.add': 0,
-      'person1Data _.change': 0,
-      'person1Data _.remove': 0,
-      'person2Data _.add': 0,
-      'person2Data _.change': 0,
-      'person2Data _.remove': 0
-    };
-
-    var PeopleCollection = fw.collection({
-      namespace: 'PeopleEventCheck',
-      dataModel: Person
-    });
-    var people = new PeopleCollection();
-    people.$namespace.subscribe('_.*', function(dataModel, envelope) {
-      var collectionEvent = envelope.topic;
-      _.each(peopleData, function(person, varName) {
-        var expectedEventKey = varName + ' ' + collectionEvent;
-        if(_.isEqual(person, dataModel.$toJS())) {
-          if(!_.isUndefined(recordedEvents[expectedEventKey])) {
-            recordedEvents[expectedEventKey]++;
-          } else {
-            if(_.isUndefined(unexpectedEvents[expectedEventKey])) {
-              unexpectedEvents[expectedEventKey] = 0;
-            }
-            unexpectedEvents[expectedEventKey]++;
-          }
-        }
-      });
-    });
-
-    expect(people().length).to.be(0);
-
-    people.$set(_.values(peopleData));
-    expect(recordedEvents).to.eql({
-      'person1Data _.add': 1,
-      'person1Data _.change': 0,
-      'person1Data _.remove': 0,
-      'person2Data _.add': 1,
-      'person2Data _.change': 0,
-      'person2Data _.remove': 0
-    });
-
-    expect(people().length).to.be(2);
-    people.$set([ peopleData.person1Data ]);
-    expect(recordedEvents).to.eql({
-      'person1Data _.add': 1,
-      'person1Data _.change': 0,
-      'person1Data _.remove': 0,
-      'person2Data _.add': 1,
-      'person2Data _.change': 0,
-      'person2Data _.remove': 1
-    });
-
-    _.extend(peopleData.person1Data, { firstName: 'changeTest1' });
-    people.$set([ peopleData.person1Data, peopleData.person2Data ]);
-    expect(recordedEvents).to.eql({
-      'person1Data _.add': 1,
-      'person1Data _.change': 1,
-      'person1Data _.remove': 0,
-      'person2Data _.add': 2,
-      'person2Data _.change': 0,
-      'person2Data _.remove': 1
-    });
-    expect(_.values(unexpectedEvents).length).to.be(0);
-  });
-
   it('can be serialized to a POJO correctly', function() {
     var person1Data = {
       "id": undefined,
@@ -215,6 +120,114 @@ describe('collection', function () {
 
     expect(people().length).to.be(2);
     expect(people.$toJSON()).to.eql(JSON.stringify(peopleData));
+  });
+
+  it('can correctly trigger change/add/remove events for dataModels in a $set() call as appropriate', function() {
+    var peopleData = {
+      "person1Data": {
+        "id": 1,
+        "firstName": "Person1FirstNameTest",
+        "lastName": "Person1LastNameTest",
+        "email": "Person1EmailTest"
+      },
+      "person2Data": {
+        "id": 2,
+        "firstName": "Person2FirstNameTest",
+        "lastName": "Person2LastNameTest",
+        "email": "Person2EmailTest"
+      }
+    };
+
+    var Person = fw.dataModel({
+      namespace: 'PersonEventCheck',
+      initialize: function(person) {
+        person = person || {};
+        this.firstName = fw.observable(person.firstName || null).mapTo('firstName');
+        this.lastName = fw.observable(person.lastName || null).mapTo('lastName');
+        this.email = fw.observable(person.email || null).mapTo('email');
+      }
+    });
+
+
+    var unexpectedEvents = {};
+    var recordedEvents = {
+      'person1Data _.add': 0,
+      'person1Data _.change': 0,
+      'person1Data _.remove': 0,
+      'person2Data _.add': 0,
+      'person2Data _.change': 0,
+      'person2Data _.remove': 0
+    };
+
+    var PeopleCollection = fw.collection({
+      namespace: 'PeopleEventCheck',
+      dataModel: Person
+    });
+    var people = new PeopleCollection();
+    people.$namespace.subscribe('_.*', function(dataModel, envelope) {
+      var collectionEvent = envelope.topic;
+      _.each(peopleData, function(person, varName) {
+        var expectedEventKey = varName + ' ' + collectionEvent;
+        var fieldNames = _.keys(person);
+        if(_.isEqual(person, _.pick(dataModel.$toJS(), fieldNames))) {
+          if(!_.isUndefined(recordedEvents[expectedEventKey])) {
+            recordedEvents[expectedEventKey]++;
+          } else {
+            if(_.isUndefined(unexpectedEvents[expectedEventKey])) {
+              unexpectedEvents[expectedEventKey] = 0;
+            }
+            unexpectedEvents[expectedEventKey]++;
+          }
+        }
+      });
+    });
+
+    expect(people().length).to.be(0);
+
+    people.$set(_.values(peopleData));
+    expect(recordedEvents).to.eql({
+      'person1Data _.add': 1,
+      'person1Data _.change': 0,
+      'person1Data _.remove': 0,
+      'person2Data _.add': 1,
+      'person2Data _.change': 0,
+      'person2Data _.remove': 0
+    });
+
+    expect(people().length).to.be(2);
+    people.$set([ peopleData.person1Data ]);
+    expect(recordedEvents).to.eql({
+      'person1Data _.add': 1,
+      'person1Data _.change': 0,
+      'person1Data _.remove': 0,
+      'person2Data _.add': 1,
+      'person2Data _.change': 0,
+      'person2Data _.remove': 1
+    });
+
+    _.extend(peopleData.person1Data, { firstName: 'changeTest1' });
+    people.$set([ peopleData.person1Data, peopleData.person2Data ]);
+    expect(recordedEvents).to.eql({
+      'person1Data _.add': 1,
+      'person1Data _.change': 1,
+      'person1Data _.remove': 0,
+      'person2Data _.add': 2,
+      'person2Data _.change': 0,
+      'person2Data _.remove': 1
+    });
+
+    peopleData.person1Data = _.omit(peopleData.person1Data, 'id');
+    people.$set([ peopleData.person1Data, peopleData.person2Data ]);
+    expect(recordedEvents).to.eql({
+      'person1Data _.add': 2,
+      'person1Data _.change': 1,
+      'person1Data _.remove': 0,
+      'person2Data _.add': 2,
+      'person2Data _.change': 0,
+      'person2Data _.remove': 1
+    });
+
+    expect(_.values(unexpectedEvents).length).to.be(0);
   });
 
   it('can correctly $fetch() data from the server and instantiate dataModels as appropriate', function(done) {
