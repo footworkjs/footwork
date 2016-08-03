5f4b5ffea55e37449d4f09d058ad3b4d2cc8de08
Jonathan Newman <jonathan@overfoc.us>
2016-08-02 21:54:40 -0500

    finished converting broadcast-receive tests

--- tests/spec/broadcast-receive.spec.js
+++ tests/spec/broadcast-receive.spec.js
@@ -16,7 +16,6 @@ define(['footwork', 'lodash', 'jquery'],
         var initializeSpy;
 
         var ModelA = fw.viewModel.create({
-          namespace: 'ModelA',
           initialize: ensureCallOrder(0, initializeSpy = jasmine.createSpy('initializeSpy', function() {
             this.broadcaster = fw.observable().broadcastAs('broadcaster');
           }).and.callThrough())
@@ -33,7 +32,6 @@ define(['footwork', 'lodash', 'jquery'],
         var initializeSpy;
 
         var ModelA = fw.viewModel.create({
-          namespace: 'ModelA',
           initialize: ensureCallOrder(0, initializeSpy = jasmine.createSpy('initializeSpy', function() {
             this.receiver = fw.observable().receiveFrom('ModelA', 'broadcaster');
           }).and.callThrough())
@@ -49,9 +47,10 @@ define(['footwork', 'lodash', 'jquery'],
       it('modelB receivable can receive data from the modelA broadcastable', function() {
         var modelAInitializeSpy;
         var modelBInitializeSpy;
+        var modelANamespaceName = fw.utils.guid();
 
         var ModelA = fw.viewModel.create({
-          namespace: 'ModelA',
+          namespace: modelANamespaceName,
           initialize: ensureCallOrder(0, modelAInitializeSpy = jasmine.createSpy('modelAInitializeSpy', function() {
             this.broadcaster = fw.observable().broadcastAs('broadcaster');
           }).and.callThrough())
@@ -62,9 +61,8 @@ define(['footwork', 'lodash', 'jquery'],
         expect(modelAInitializeSpy).toHaveBeenCalled();
 
         var ModelB = fw.viewModel.create({
-          namespace: 'ModelB',
           initialize: ensureCallOrder(1, modelBInitializeSpy = jasmine.createSpy('modelBInitializeSpy', function() {
-            this.receiver = fw.observable().receiveFrom('ModelA', 'broadcaster');
+            this.receiver = fw.observable().receiveFrom(modelANamespaceName, 'broadcaster');
           }).and.callThrough())
         });
 
@@ -108,6 +106,176 @@ define(['footwork', 'lodash', 'jquery'],
         var broadcastable = fw.observable('testValue').broadcastAs({ name: 'broadcaster', namespace: namespace });
 
         expect(receivable()).toBe(broadcastable());
+      });
+
+      it('modelB can write to a receivable and modelA sees the new data on a writable broadcastable', function() {
+        var modelAInitializeSpy;
+        var modelBInitializeSpy;
+        var modelANamespaceName = fw.utils.guid();
+
+        var ModelA = fw.viewModel.create({
+          namespace: modelANamespaceName,
+          initialize: ensureCallOrder(0, modelAInitializeSpy = jasmine.createSpy('modelAInitializeSpy', function() {
+            this.writableBroadcaster = fw.observable().broadcastAs('writableBroadcaster', true);
+          }).and.callThrough())
+        });
+
+        expect(modelAInitializeSpy).not.toHaveBeenCalled();
+        var modelA = new ModelA();
+        expect(modelAInitializeSpy).toHaveBeenCalled();
+
+        var ModelB = fw.viewModel.create({
+          initialize: ensureCallOrder(1, modelBInitializeSpy = jasmine.createSpy('modelBInitializeSpy', function() {
+            this.writableReceiver = fw.observable().receiveFrom(modelANamespaceName, 'writableBroadcaster', true);
+          }).and.callThrough())
+        });
+
+        expect(modelBInitializeSpy).not.toHaveBeenCalled();
+        var modelB = new ModelB();
+        expect(modelBInitializeSpy).toHaveBeenCalled();
+
+        var testValue = 'a-different-specific-value';
+        modelB.writableReceiver(testValue);
+
+        expect(modelA.writableBroadcaster()).toBe(testValue);
+      });
+
+      it('when modelB tries to write to receivable modelA does not see the data on a non-writable broadcastable and the receivable is not set to the new value', function() {
+        var modelAInitializeSpy;
+        var modelBInitializeSpy;
+        var modelANamespaceName = fw.utils.guid();
+
+        var ModelA = fw.viewModel.create({
+          namespace: modelANamespaceName,
+          initialize: ensureCallOrder(0, modelAInitializeSpy = jasmine.createSpy('modelAInitializeSpy', function() {
+            this.nonwritableBroadcaster = fw.observable().broadcastAs('nonwritableBroadcaster');
+          }).and.callThrough())
+        });
+
+        expect(modelAInitializeSpy).not.toHaveBeenCalled();
+        var modelA = new ModelA();
+        expect(modelAInitializeSpy).toHaveBeenCalled();
+
+        var ModelB = fw.viewModel.create({
+          initialize: ensureCallOrder(1, modelBInitializeSpy = jasmine.createSpy('modelBInitializeSpy', function() {
+            this.nonwritableReceiver = fw.observable().receiveFrom(modelANamespaceName, 'nonwritableBroadcaster', true);
+          }).and.callThrough())
+        });
+
+        expect(modelBInitializeSpy).not.toHaveBeenCalled();
+        var modelB = new ModelB();
+        expect(modelBInitializeSpy).toHaveBeenCalled();
+
+        var testValue = 'specific-value-that-should-not-be-seen';
+        modelB.nonwritableReceiver(testValue);
+        expect(modelB.nonwritableReceiver()).not.toBe(testValue);
+        expect(modelA.nonwritableBroadcaster()).not.toBe(testValue);
+      });
+
+      it('receivable with .when() specified writes when callback returns true', function() {
+        var modelAInitializeSpy;
+        var modelBInitializeSpy;
+        var whenSpy;
+        var modelANamespaceName = fw.utils.guid();
+
+        var ModelA = fw.viewModel.create({
+          namespace: modelANamespaceName,
+          initialize: ensureCallOrder(0, modelAInitializeSpy = jasmine.createSpy('modelAInitializeSpy', function() {
+            this.broadcaster = fw.observable().broadcastAs('broadcasterToTestWhenCallback');
+          }).and.callThrough())
+        });
+
+        expect(modelAInitializeSpy).not.toHaveBeenCalled();
+        var modelA = new ModelA();
+        expect(modelAInitializeSpy).toHaveBeenCalled();
+
+        var ModelB = fw.viewModel.create({
+          initialize: ensureCallOrder(1, modelBInitializeSpy = jasmine.createSpy('modelBInitializeSpy', function() {
+            this.receiver = fw.observable().receiveFrom(modelANamespaceName, 'broadcasterToTestWhenCallback').when(ensureCallOrder(2, whenSpy = jasmine.createSpy('whenSpy', function() {
+              return true;
+            }).and.callThrough()));
+          }).and.callThrough())
+        });
+
+        expect(modelBInitializeSpy).not.toHaveBeenCalled();
+        var modelB = new ModelB();
+        expect(whenSpy).not.toHaveBeenCalled();
+        expect(modelBInitializeSpy).toHaveBeenCalled();
+
+        var testValue = 'value-that-should-be-visible-from-ModelB';
+        modelA.broadcaster(testValue);
+        expect(whenSpy).toHaveBeenCalled();
+        expect(modelB.receiver()).toBe(testValue);
+      });
+
+      it('receivable with .when() specified does NOT write when callback returns false', function() {
+        var modelAInitializeSpy;
+        var modelBInitializeSpy;
+        var whenSpy;
+        var modelANamespaceName = fw.utils.guid();
+
+        var ModelA = fw.viewModel.create({
+          namespace: modelANamespaceName,
+          initialize: ensureCallOrder(0, modelAInitializeSpy = jasmine.createSpy('modelAInitializeSpy', function() {
+            this.broadcaster = fw.observable().broadcastAs('broadcasterToTestWhenCallback');
+          }).and.callThrough())
+        });
+
+        expect(modelAInitializeSpy).not.toHaveBeenCalled();
+        var modelA = new ModelA();
+        expect(modelAInitializeSpy).toHaveBeenCalled();
+
+        var ModelB = fw.viewModel.create({
+          initialize: ensureCallOrder(1, modelBInitializeSpy = jasmine.createSpy('modelBInitializeSpy', function() {
+            this.receiver = fw.observable().receiveFrom(modelANamespaceName, 'broadcasterToTestWhenCallback').when(ensureCallOrder(2, whenSpy = jasmine.createSpy('whenSpy', function() {
+              return false;
+            }).and.callThrough()));
+          }).and.callThrough())
+        });
+
+        expect(modelBInitializeSpy).not.toHaveBeenCalled();
+        var modelB = new ModelB();
+        expect(whenSpy).not.toHaveBeenCalled();
+        expect(modelBInitializeSpy).toHaveBeenCalled();
+
+        modelA.broadcaster('value-that-should-NOT-be-visible-from-ModelB');
+        expect(whenSpy).toHaveBeenCalled();
+        expect(modelB.receiver()).toBe(undefined);
+      });
+
+      it('receivable with .when() sees correct value passed to it in the callback', function() {
+        var modelAInitializeSpy;
+        var modelBInitializeSpy;
+        var whenSpy;
+        var modelANamespaceName = fw.utils.guid();
+        var testValue = 'value-that-should-be-visible-from-ModelB-callback';
+
+        var ModelA = fw.viewModel.create({
+          namespace: modelANamespaceName,
+          initialize: ensureCallOrder(0, modelAInitializeSpy = jasmine.createSpy('modelAInitializeSpy', function() {
+            this.broadcaster = fw.observable().broadcastAs('broadcasterToTestWhenCallback');
+          }).and.callThrough())
+        });
+
+        expect(modelAInitializeSpy).not.toHaveBeenCalled();
+        var modelA = new ModelA();
+        expect(modelAInitializeSpy).toHaveBeenCalled();
+
+        var ModelB = fw.viewModel.create({
+          initialize: ensureCallOrder(1, modelBInitializeSpy = jasmine.createSpy('modelBInitializeSpy', function() {
+            this.receiver = fw.observable().receiveFrom(modelANamespaceName, 'broadcasterToTestWhenCallback').when(ensureCallOrder(2, whenSpy = jasmine.createSpy('whenSpy', function(val) {
+              expect(val).toBe(testValue);
+            }).and.callThrough()));
+          }).and.callThrough())
+        });
+
+        expect(modelBInitializeSpy).not.toHaveBeenCalled();
+        new ModelB();
+        expect(whenSpy).not.toHaveBeenCalled();
+        expect(modelBInitializeSpy).toHaveBeenCalled();
+
+        modelA.broadcaster(testValue);
+        expect(whenSpy).toHaveBeenCalled();
       });
     });
   }
