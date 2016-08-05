e3b03436f1267e26cae14a52f308e9366549c657
Jonathan Newman <jonathan@overfoc.us>
2016-08-05 18:47:22 -0500

    debug output

--- tests/assets/fixtures/defaultComponentLocation/default-component-location.js
+++ tests/assets/fixtures/defaultComponentLocation/default-component-location.js
@@ -1,6 +1,7 @@
 define(['footwork'], function(fw) {
   return fw.viewModel.create({
     initialize: function() {
+      console.log('defaultComponentLocationLoaded');
       window.defaultComponentLocationLoaded = true;
     }
   });
