c006b5de11a1408c6298e7752695a3c93af24905
Jonathan Newman <jonathan@overfoc.us>
2016-08-05 18:31:02 -0500

    fixed browsers ref

--- karma-sauce.conf.js
+++ karma-sauce.conf.js
@@ -113,10 +113,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: [
-        'Chrome',
-        'Firefox'
-    ],
+    browsers: Object.keys(customLaunchers),
 
 
     customLaunchers: customLaunchers,
