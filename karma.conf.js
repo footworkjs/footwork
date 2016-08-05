99ef9118913963bc6887df829e925d984a9548a8
Jonathan Newman <jonathan@overfoc.us>
2016-08-04 21:10:00 -0500

    WIP saucelabs integration

--- karma.conf.js
+++ karma.conf.js
@@ -1,4 +1,32 @@
 module.exports = function(config) {
+  // Use ENV vars on Travis and sauce.json locally to get credentials
+  if (!process.env.SAUCE_USERNAME) {
+    if (!fs.existsSync('sauce.json')) {
+      console.log('Create a sauce.json with your credentials based on the sauce-sample.json file.');
+      process.exit(1);
+    } else {
+      process.env.SAUCE_USERNAME = require('./sauce').username;
+      process.env.SAUCE_ACCESS_KEY = require('./sauce').accessKey;
+    }
+  }
+
+  // Browsers to run on Sauce Labs
+  var customLaunchers = {
+    'SL_Chrome': {
+      base: 'SauceLabs',
+      browserName: 'chrome'
+    },
+    'SL_InternetExplorer': {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      version: '10'
+    },
+    'SL_FireFox': {
+      base: 'SauceLabs',
+      browserName: 'firefox',
+    }
+  };
+
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
@@ -49,7 +77,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['spec', 'coverage'],
+    reporters: ['spec', 'coverage', 'saucelabs'],
 
     coverageReporter: {
       dir : 'build/coverage/',
@@ -71,6 +99,13 @@ module.exports = function(config) {
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_WARN,
+
+
+    sauceLabs: {
+      testName: 'Footwork Framework Tests'
+    },
+    captureTimeout: 120000,
+    customLaunchers: customLaunchers,
 
 
     // enable / disable watching file and executing tests whenever any file changes
