06362d9c4553e390418cf2659e94727ce8d32544
Jonathan Newman <jonathan@overfoc.us>
2016-08-04 21:56:00 -0500

    added saucelabs reporter to gulpfile task

--- gulpfile.js
+++ gulpfile.js
@@ -36,6 +36,32 @@ var banner = [
   '', ''
 ];
 
+var customLaunchers = {
+  sl_chrome: {
+    base: 'SauceLabs',
+    browserName: 'chrome',
+    platform: 'Windows 7',
+    version: '35'
+  },
+  sl_firefox: {
+    base: 'SauceLabs',
+    browserName: 'firefox',
+    version: '30'
+  },
+  sl_ios_safari: {
+    base: 'SauceLabs',
+    browserName: 'iphone',
+    platform: 'OS X 10.9',
+    version: '7.1'
+  },
+  sl_ie_11: {
+    base: 'SauceLabs',
+    browserName: 'internet explorer',
+    platform: 'Windows 8.1',
+    version: '11'
+  }
+};
+
 function buildRelease(buildProfile) {
   var headerBanner = banner.slice(0).join('\n');
   var pkgData = pkg;
@@ -69,7 +95,12 @@ gulp.task('default', ['ci', 'copy_animation_styles_to_build']);
 function runTests(done) {
   return new Server({
     configFile: __dirname + '/karma.conf.js',
-    singleRun: true
+    singleRun: true,
+    sauceLabs: {
+      testName: "Footwork unit tests"
+    },
+    reporters: ['saucelabs'],
+    customLaunchers: customLaunchers
   }, done).start();
 }
 
