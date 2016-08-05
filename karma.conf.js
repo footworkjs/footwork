81c3bff11e414cd4db2bfd49c764bdec3764c6c1
Jonathan Newman <jonathan@overfoc.us>
2016-08-04 21:13:42 -0500

    fixed typo in and cleaned up karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,14 +1,8 @@
+var fs = require('fs');
+
 module.exports = function(config) {
-  // Use ENV vars on Travis and sauce.json locally to get credentials
-  if (!process.env.SAUCE_USERNAME) {
-    if (!fs.existsSync('sauce.json')) {
-      console.log('Create a sauce.json with your credentials based on the sauce-sample.json file.');
-      process.exit(1);
-    } else {
-      process.env.SAUCE_USERNAME = require('./sauce').username;
-      process.env.SAUCE_ACCESS_KEY = require('./sauce').accessKey;
-    }
-  }
+  process.env.SAUCE_USERNAME = require('./sauce').username;
+  process.env.SAUCE_ACCESS_KEY = require('./sauce').accessKey;
 
   // Browsers to run on Sauce Labs
   var customLaunchers = {
