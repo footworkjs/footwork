b2cfae856ebf4ad0cb51de7a645a399bb1dd5306
Jonathan Newman <jonathan@overfoc.us>
2016-08-06 14:25:08 -0500

    test fix for failing tests

--- tests/assets/fixtures/AMDDataModel.js
+++ tests/assets/fixtures/AMDDataModel.js
@@ -1,8 +1,6 @@
 define(['footwork'], function(fw) {
   return fw.dataModel.create({
     namespace: 'AMDDataModel',
-    initialize: registerEntity('AMDDataModel', function() {
-      console.log('AMDDataModel registered');
-    })
+    initialize: registerFootworkEntity()
   });
 });
