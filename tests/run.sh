10ba7c2a34c4f4f962efed26e6c4dcff304a90ef
Jonathan Newman <jonathan@overfoc.us>
2016-08-07 19:48:54 -0500

    fixed permissions and added test env notification output

--- tests/run.sh
+++ tests/run.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 if [ "$JOB" = "smoke" ]; then
+  echo "Test Environment: smoke"
   gulp tests
 elif [ "$JOB" = "sauce" ]; then
+  echo "Test Environment: SauceLabs"
   gulp coveralls
 fi
