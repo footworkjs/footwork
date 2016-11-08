35e194ccd532ae12d47e8f5096684c215057b7a1
Jonathan Newman <jonathan@overfoc.us>
2016-11-08 06:39:13 -0600

    test adjustments

--- source/misc/config.js
+++ source/misc/config.js
@@ -1,6 +1,5 @@
 module.exports = {
   entityClass: 'fw-entity',
   entityAnimateClass: 'fw-entity-resolved',
-  defaultNamespace: '__fwDefault',
   privateDataSymbol: require('./util').getSymbol('footwork')
 };
