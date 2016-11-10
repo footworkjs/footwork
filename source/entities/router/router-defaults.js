1ef213ebe4e7f584ed4583e1a34eb13ba881f1d5
Jonathan Newman <jonathan@overfoc.us>
2016-11-10 15:58:02 -0600

    cleaned up router-defaults.js

--- source/entities/router/router-defaults.js
+++ source/entities/router/router-defaults.js
@@ -1,27 +1,14 @@
 var fw = require('knockout/build/output/knockout-latest');
-var _ = require('footwork-lodash');
-
-var config = require('../../misc/config');
-var entityAnimateClass = config.entityAnimateClass;
-var privateDataSymbol = config.privateDataSymbol;
-
-var getSymbol = require('../../misc/util').getSymbol;
-
-var noComponentSelected = '_noComponentSelected';
-var nullComponent = '_nullComponent';
-var invalidRoutePathIdentifier = '___invalid-route';
-
-var routesAreCaseSensitive = true;
 
 var nullRouter = {};
-nullRouter[getSymbol('isNullRouter')] = true;
+nullRouter[require('../../misc/util').getSymbol('isNullRouter')] = true;
 
 module.exports = {
   defaultLoadingComponent: 'default-loading-display',
-  noComponentSelected: noComponentSelected,
-  nullComponent: nullComponent,
-  invalidRoutePathIdentifier: invalidRoutePathIdentifier,
-  routesAreCaseSensitive: routesAreCaseSensitive,
+  noComponentSelected: '_noComponentSelected',
+  nullComponent: '_nullComponent',
+  invalidRoutePathIdentifier: '___invalid-route',
+  routesAreCaseSensitive: true,
   nullRouter: nullRouter,
   outletLoadingDisplay: 'fw-loading-display',
   outletLoadedDisplay: 'fw-loaded-display',
