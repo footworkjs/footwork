4e6dd32705d651c4255f85a23bc0992229bf3c1e
Jonathan Newman <jonathan@overfoc.us>
2016-12-12 07:11:23 -0600

    fixed issue with navigator check causing failure when running directly in node

--- source/binding/binding-element.js
+++ source/binding/binding-element.js
@@ -11,16 +11,18 @@ var bindingElement = {
 
 /* istanbul ignore next */
 (function checkForOldIE () {
-  var clientNavigator = navigator.userAgent.toLowerCase();
-  var exploderVersion = (clientNavigator.indexOf('msie') != -1) ? parseInt(clientNavigator.split('msie')[1]) : (oldExploderVersion + 1);
+  if(typeof navigator !== 'undefined') {
+    var clientNavigator = navigator.userAgent.toLowerCase();
+    var exploderVersion = (clientNavigator.indexOf('msie') != -1) ? parseInt(clientNavigator.split('msie')[1]) : (oldExploderVersion + 1);
 
-  if (exploderVersion <= oldExploderVersion) {
-    // client is using an older IE, we have to initialize the wrapper with a normal element (ie < 9 doesn't support virtual elements)
-    bindingElement.open = {
-      prefix: '<' + bindingElement.oldExploderTagName + ' class="' + bindingElement.oldExploderClassName + '" data-bind="',
-      postfix: '">'
-    };
-    bindingElement.close = '</' + bindingElement.oldExploderTagName + '>';
+    if (exploderVersion <= oldExploderVersion) {
+      // client is using an older IE, we have to initialize the wrapper with a normal element (ie < 9 doesn't support virtual elements)
+      bindingElement.open = {
+        prefix: '<' + bindingElement.oldExploderTagName + ' class="' + bindingElement.oldExploderClassName + '" data-bind="',
+        postfix: '">'
+      };
+      bindingElement.close = '</' + bindingElement.oldExploderTagName + '>';
+    }
   }
 })();
 
